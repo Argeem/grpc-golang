3480a7a972d911e289428c27572ad1e841e2574e
Sitthisak Bannob <t-sitthisak.ban@axons.in.th>
2023-01-24 13:01:15 +0700

    add tls credentials to server and client

--- client/main.go
+++ client/main.go
@@ -2,15 +2,34 @@ package main
 
 import (
 	"client/services"
+	"flag"
 	"log"
 
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
 	"google.golang.org/grpc/credentials/insecure"
 )
 
 func main() {
-	creds := insecure.NewCredentials()
-	cc, err := grpc.Dial("localhost:50051", grpc.WithTransportCredentials(creds))
+	var cc *grpc.ClientConn
+	var err error
+	var creds credentials.TransportCredentials
+
+	host := flag.String("host", "localhost:50051", "gRPC server host")
+	tls := flag.Bool("tls", false, "use a secure TLS connection")
+	flag.Parse()
+
+	if *tls {
+		certFile := "../tls/ca.crt"
+		creds, err = credentials.NewClientTLSFromFile(certFile, "")
+		if err != nil {
+			log.Fatal(err)
+		}
+	} else {
+		creds = insecure.NewCredentials()
+	}
+
+	cc, err = grpc.Dial(*host, grpc.WithTransportCredentials(creds))
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -18,9 +37,9 @@ func main() {
 	calculatorClient := services.NewCalculatorClient(cc)
 	calculatorService := services.NewCalculatorService(calculatorClient)
 
-	//err = calculatorService.Hello("sitthisak")
+	// err = calculatorService.Hello("sitthisak")
 	// err = calculatorService.Fibonacci(5)
-	//err = calculatorService.Average(1, 2, 3, 4, 5)
+	// err = calculatorService.Average(1, 2, 3, 4, 5)
 	err = calculatorService.Sum(1, 2, 3, 4, 5)
 	if err != nil {
 		log.Fatal(err)
